2d2ffd33b8d17d53bd33cc447bca135f05eb739a
Eunji Lee <ji0859@kookmin.ac.kr>
2017-09-10 18:13:09 +0900

    -1일 경우 처리, 소수 처리 해야함

--- week2/factorial.py
+++ week2/factorial.py
@@ -1,15 +1,19 @@
-def fac(a):
+#팩토리얼 구하는 함수 
+def fac(a): 
     if a == 0:
         return 1
-    return a * fac(a-1)
+    return a * fac(a - 1) 
 
+# 반복적으로 돌려줌 
 while True:
     n = str(input())
-    if int(n) < 0:
-        if n == -1 :
-            break
-        print ("양의 정수를 입력하시오")
-    elif type(n) == float:
+    '''if float(n) == n:
         print ("정수를 입력하시오")
-    else :
-        print (fac(int(n))) 
+        float(n) == int(n)'''
+    if int(n) == -1: #-1일 경우 함수를 빠져나옴 
+        break
+    elif int(n) < 0: #예외처리
+        print("양의 정수를 입력하시오")
+
+    else:
+        print(fac(int(n))) 
