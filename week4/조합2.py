c540dc2d1a363b02d4c1b3d84ff074e28c61cad1
Eunji Lee <ji0859@kookmin.ac.kr>
2017-10-11 00:20:48 +0900

    factorial을 이용하지 않은 조별토론 이후 수정한 코드

--- week4/조합2.py
+++ week4/조합2.py
@@ -1,20 +1,16 @@
 #팩토리얼을 이용하지 않고 구하기
-# 구현하지 못함
-def fact(n, m):
-    sum = 1
-    while n >= m:
-        sum *=n
-        n -=1
-        return n
-    return sum
-
-def fac(j):
-    um = 1
-    for k in range(j +1):
-        k +=1
-        um *=k
-    return um
-a = int(input("nCr 의 n을 입력하시오."))
-b = int(input("nCr 의 r을 입력하시오."))
-c  = print (fact(a, b))
-print (c/fact(b))
+def combi(n,r):
+      if r == 0:
+            return 1
+      elif n < r:
+            return 0
+      else:
+            return combi(n-1, r-1) + combi(n-1, r)
+while True:
+	n = int(input("첫번째 수를 입력 또는 -1을 입력하면 종료: "))
+	if n == -1:
+		break
+	r = int(input("두번째 수를 입력:" ))
+	if n <= 0 and r < 0:
+		continue
+	print(int(combi(n,r)))
