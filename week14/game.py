90cc500483987c289a1d04f1718d9c753b43c8c7
Eunji Lee <ji0859@kookmin.ac.kr>
2017-12-05 01:02:43 +0900

    main code remaining lives

---
+++ week14/game.py
@@ -0,0 +1,48 @@
+from hangman import Hangman
+from guess import Guess
+from word import Word
+
+
+def gameMain():
+    word = Word('words.txt')
+    guess = Guess(word.randFromDB())
+    hangman = Hangman()
+
+    while hangman.remainingLives > 0:
+
+        display = hangman.currentShape()
+        print(display)
+        display = guess.displayCurrent()
+        print('Current: ' + display)
+        display = guess.displayGuessed()
+        print('Already Used: ' + display)
+        #display = hangman.decreaseLife()
+        #print("remainingLives:" , display)
+        guessedChar = input('Select a letter: ')
+        if len(guessedChar) != 1:
+            print('One character at a time!')
+            continue
+        if guessedChar in guess.guessedChars:
+            print('You already guessed \"' + guessedChar + '\"')
+            continue
+
+        success = guess.guess(guessedChar)
+        if success == False:
+            print("")
+            print("remaining life:" , hangman.decreaseLife())
+        
+        if guess.finished():
+            break
+
+    if guess.finished() == True:
+        print('**** ' + guess.displayCurrent() + ' ****')
+        print('Success')
+    else:
+        print(hangman.currentShape())
+        print('word [' + guess.secretWord + ']')
+        print('guess [' + guess.displayCurrent() + ']')
+        print('Fail')
+
+
+if __name__ == '__main__':
+    gameMain()
